18074a131380f523b35e1c5671beca484c6c211c
eadmaster <925171+eadmaster@users.noreply.github.com>
2026-05-05 03:50:12 +0200

    starfy4: added map legend translation, revised some text

--- examples/Stafy 4 NDS/_build_fixed.sh
+++ examples/Stafy 4 NDS/_build_fixed.sh
@@ -3,7 +3,7 @@
 INPUT_ROM="Densetsu no Stafy 4 (Japan).nds"
 OUTPUT_ROM="Densetsu no Stafy 4 (English).nds"
 
-replace_gfx() {
+replace_cell_gfx() {
     [ -f gfx/$1.NCGR ] && cp -fv gfx/$1.NCGR  "Densetsu no Stafy 4 (Japan)/data/Cell/$1.NCGR"
     [ -f gfx/$1.NCER ] && cp -fv gfx/$1.NCER  "Densetsu no Stafy 4 (Japan)/data/Cell/$1.NCER"
 }
@@ -16,16 +16,16 @@ replace_gfx() {
 cp Font/stafy4_13b2_full_bmp_comb_clean.bin  "Densetsu no Stafy 4 (Japan)/data/Font/stafy4_13b2_full_bmp.bin"
 cp Font/stafy4_13b2_2_bmp_comb_clean.bin  "Densetsu no Stafy 4 (Japan)/data/Font/stafy4_13b2_2_bmp.bin"
 
-replace_gfx title_ue_ob_font  # "Press Start!" on title screen
-replace_gfx title_sita_ob_font  # "Start" on title screen
-replace_gfx save_window_l  # "Save now?" window (used in stages)
-replace_gfx save_obj  # "Save now?" window (autosave after bosses)
-replace_gfx city1_obj_sub_l  # 1st city map ui elements
-replace_gfx fileselect_low_obj  # File menu on title screen
-replace_gfx city_in_l  # "[Back] To City" sign in stages
-replace_gfx pose_obj_l  # Pause menu
-replace_gfx fukidashi_00_l  # Press Button X
-replace_gfx gameover_obj_1  # Game Over menu: Retry / Quit
+replace_cell_gfx title_ue_ob_font  # "Press Start!" on title screen
+replace_cell_gfx title_sita_ob_font  # "Start" on title screen
+replace_cell_gfx save_window_l  # "Save now?" window (used in stages)
+replace_cell_gfx save_obj  # "Save now?" window (autosave after bosses)
+replace_cell_gfx city1_obj_sub_l  # 1st city map ui elements
+replace_cell_gfx fileselect_low_obj  # File menu on title screen
+replace_cell_gfx city_in_l  # "[Back] To City" sign in stages
+replace_cell_gfx pose_obj_l  # Pause menu
+replace_cell_gfx fukidashi_00_l  # Press Button X
+replace_cell_gfx gameover_obj_1  # Game Over menu: Retry / Quit
 
 # replace boss speech stored as gfx
 cd gfx
@@ -42,6 +42,12 @@ for f in jizou_st*_l.NCGR jizou_st*_l.NCER; do
     cp -fv "$f" "../Densetsu no Stafy 4 (Japan)/data/Stage$NUMBER/"
 done
 cd ..
+
+# extract and patch map legend embedded gfx from overlay
+#sfk partcopy "Densetsu no Stafy 4 (Japan)/overlay/main_0013.bin" 0x11B810 0x2426 gfx/811B810_lzss_jap.bin -yes  # extract original
+# sfk partcopy "Densetsu no Stafy 4 (Japan)/overlay/main_0013.bin" 0x11DC38 0x1A9 811DC38_lzss.bin -yes  # palette
+# cp /r/811B810_lzss_eng.bin . ; truncate --reference 811B810_lzss_jap.bin 811B810_lzss_eng.bin
+sfk partcopy gfx/811B810_lzss_eng.bin 0x0 0x2426 "Densetsu no Stafy 4 (Japan)/overlay/main_0013.bin" 0x11B810  -yes  # patch inside the overlay file
 
 # repack with custom font and gfx with NitroPacker https://github.com/haroohie-club/NitroPacker
 NitroPacker pack -p "Densetsu no Stafy 4 (Japan)/Densetsu no Stafy 4 (English).json" -r "$OUTPUT_ROM"
