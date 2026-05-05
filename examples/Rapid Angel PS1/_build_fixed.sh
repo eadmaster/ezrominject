a737f1e7e06c76d672fe790ba44b180cf783ae4a
eadmaster <925171+eadmaster@users.noreply.github.com>
2026-05-05 22:20:28 +0200

    rapid angel: added full script

--- examples/Rapid Angel PS1/_build_fixed.sh
+++ examples/Rapid Angel PS1/_build_fixed.sh
@@ -4,9 +4,9 @@ OUTPUT_ROM="Kaisoku Tenshi - The Rapid Angel (Japan) (Track 1) (patched).bin"
 
 cp "$INPUT_ROM" "$OUTPUT_ROM"
 
-python ../../ezrominject.py *_jap.txt *_eng.txt "$OUTPUT_ROM"
+python ../../ezrominject.py *_jap.txt *_eng_scenes.txt "$OUTPUT_ROM"  # cutscenes using internal font
+python ../../ezrominject.py *_jap.txt *_eng.txt "$OUTPUT_ROM" --ascii-bios-hack  # dialogues using bios font
 #NOT COMPAT:  --ascii-mode
-#NOT COMPAT: --ascii-bios-hack  (only dialogues)
  
 xdelta3 -S none -f -e -s "$INPUT_ROM" "$OUTPUT_ROM"  "$OUTPUT_ROM.xdelta"
 
